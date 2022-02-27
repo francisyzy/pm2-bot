707899e25858ed9fb77024740e826fd84c5e236e
francisyzy <francisyzy@me.com>
2022-02-27 18:01:50 +0800

    Update envs

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,9 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || "3000",
-  DATABASE_URL: process.env.DATABASE_URL,
   API_TOKEN: process.env.API_TOKEN,
   LOG_GROUP_ID: process.env.LOG_GROUP_ID,
-  URL: process.env.URL,
+  ADMIN_ID: Number(process.env.ADMIN_ID) || 0,
 };
 
 export default config;
